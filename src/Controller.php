3e2909928b42bf66b2e5ba16de8800aa4df32a61
hau <nihonggo2004@gmail.com>
2023-11-16 09:24:33 +0700

    a

--- src/Controller.php
+++ src/Controller.php
@@ -2,32 +2,37 @@
 
 namespace Ductong\BaseMvc;
 
-class Controller {
-    protected function renderAuthor($view, $data = []) {
+class Controller
+{
+    protected function renderAuthor($view, $data = [])
+    {
         include_once 'Views/components/layout/author/header.php';
         extract($data);
 
         include "Views/$view.php";
-        include_once 'Views/components/layout/author/footer.php'; 
+        include_once 'Views/components/layout/author/footer.php';
     }
-    
-    protected function renderAdmin($view, $data = []) {
+
+    protected function renderAdmin($view, $data = [])
+    {
         include_once 'Views/components/layout/admin/header.php';
-        
+
         extract($data);
 
         include "Views/admin/$view.php";
 
         include_once 'Views/components/layout/admin/footer.php';
     }
-    
 
 
 
-    protected function renderClient($view, $data = []) {
+
+    protected function renderClient($view, $data = [])
+    {
         include_once 'Views/components/layout/client/header.php';
 
         extract($data);
+
         include "Views/$view.php";
         include_once 'Views/components/layout/client/footer.php';
     }
---
+++ src/Views/client/index.php
@@ -0,0 +1,8 @@
+<div class="container_content">
+    <div class="banner">
+    <img src="src/Views/components/layout/client/img/banner.png" alt="">
+    </div>
+    <div class="bannerphu">
+    <img src="src/Views/components/layout/client/img/banner1.png" alt="">
+    </div>
+</div>
