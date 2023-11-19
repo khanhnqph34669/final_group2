49b74934f10a812dcf312daec2ff99db0159c280
hau <nihonggo2004@gmail.com>
2023-11-19 10:24:51 +0700

    push

---
+++ src/Views/client/tintuc.php
@@ -0,0 +1,10 @@
+<div class="container-tintuc">
+    <div class="tinchinh">
+        <div class="tin">
+            <img src="../src/Views/components/layout/client/img/image1.png" alt="">
+            <p>ajhdhagsv </p>
+            <p>Ngày 20/11/2023</p>
+        </div>
+    </div>
+    <div class="tinphu"></div>
+</div>
