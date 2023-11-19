15016a256ee79ed0caed811beab2b4c0b5017633
hau <nihonggo2004@gmail.com>
2023-11-19 10:39:43 +0700

    a

--- src/Views/client/tintuc.php
+++ src/Views/client/tintuc.php
@@ -2,7 +2,6 @@
     <div class="tinchinh">
         <div class="tin">
             <img src="../src/Views/components/layout/client/img/image1.png" alt="">
-            <p>ajhdhagsv </p>
             <p>ádjhagdh</p>
             <p>Ngày 20/11/2023</p>
         </div>
