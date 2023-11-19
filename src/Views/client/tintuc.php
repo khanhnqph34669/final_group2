7a10e534e3c56372c0a0ad59cf9e47fbb51123d4
hau <nihonggo2004@gmail.com>
2023-11-19 10:34:00 +0700

    1

---
+++ src/Views/client/tintuc.php
@@ -0,0 +1,12 @@
+<div class="container-tintuc">
+    <div class="tinchinh">
+        <div class="tin">
+            <img src="../src/Views/components/layout/client/img/image1.png" alt="">
+            <p>ajhdhagsv </p>
+            <p>ádjhagdh</p>
+            <p>Ngày 20/11/2023</p>
+            <p>háhgdj</p>
+        </div>
+    </div>
+    <div class="tinphu"></div>
+</div>
