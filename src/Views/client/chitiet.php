2f7c947a52e5791959ffb5a5dd3d4e9cec0992c1
hau <nihonggo2004@gmail.com>
2023-11-25 10:49:59 +0700

    a

--- src/Views/client/chitiet.php
+++ src/Views/client/chitiet.php
@@ -2,8 +2,9 @@
     <div class="noidungtt">
         <div class="tinchinh space20">
             <div class=" space20">
-                <p>Ngày 20/11/2023</p>,
+
                 <h2 class="cttieude">Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước </h2>
+                <p>Ngày 20/11/2023</p>
                 <img class="imgNews2" src="../src/Views/components/layout/client/img/image1.png" alt="">
                 <div class="noidung">
 
@@ -20,33 +21,6 @@
                 <h2>Tin Tức 1</h2>
                 <a href="">
                     <div class="tinphu tin space20">
-                        <div class="tieude">,
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
                         <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
                         <div class="tieude">
                             <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
@@ -55,45 +29,7 @@
                     </div>
                 </a>
             </div>
-            <div class="tinphu space20">
-                <h2>Tin Tức 2</h2>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+
 
 
         </div>
@@ -112,49 +48,6 @@
 
             </div>
         </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
 
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
     </div>
 </div>
--- src/Views/client/technology.php
+++ src/Views/client/technology.php
@@ -10,6 +10,7 @@
                         <div class="tieude">
                             <h3><?= $post['Title'] ?></h3>
                             <p><?= $post['Content'] ?></p>
+                            <p>...</p>
                             <a href="/client/post/preview?id=<?= $post['Id'] ?>" class="btn btn-info space20">Xem trước</a>
                         </div>
                     </div>
@@ -29,73 +30,8 @@
                         </div>
                     </div>
                 </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
             </div>
-            <div class="tinphu space20">
-                <h2>Tin Tức 2</h2>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="tinphu tin space20">
-                        <img class="imgNews1" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                        <div class="tieude">
-                            <h5>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</h5>
-                            <p>Ngày 20/11/2023</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+
 
 
         </div>
@@ -114,49 +50,6 @@
 
             </div>
         </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
 
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
-        <a href="">
-            <div class="tinlienquan1 space20">
-                <div class=" space20">
-                    <img class="imgNews" src="../src/Views/components/layout/client/img/image1.png" alt="">
-                    <div class="tieude1">
-                        <p>Không cần mang theo giấy phép lái xe, đăng ký xe…nếu đã được tích hợp vào Thẻ căn cước</p>
-                    </div>
-                </div>
-
-            </div>
-        </a>
     </div>
 </div>
